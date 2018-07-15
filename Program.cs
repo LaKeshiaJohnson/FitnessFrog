b6273fece78963b5f2e864f1e77517aee4ecda25
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-07-15 11:43:26 -0500

    starter file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+class Program
+{
+	static void Main()
+	{
+		//console app
+		//Prompt user for minutes exercised
+		System.Console.Write("Enter how many minutes you exercised: ");
+		//Add minutes to total
+		//Display total minutes exercised to the screen
+		//Repeat until the user quits
+	}
+}
