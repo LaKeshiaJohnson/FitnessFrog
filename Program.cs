ad614fee8a71280122b0afb937e740981bdd6bd4
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-07-16 19:10:06 -0500

    basic functionality

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
-class Program
+//console app
+
+using System;
+//allows to avoid repeating the word system thoughout the code
+
+//Goal:
+//1. Prompt user for minutes exercised
+//2. Add minutes to total
+//3.Display total minutes exercised to the screen
+//4.Repeat until the user quits
+
+namespace FitnessFrog
 {
-	static void Main()
+	class Program
 	{
-		//console app
-		//Prompt user for minutes exercised
-		System.Console.Write("Enter how many minutes you exercised: ");
-		//Add minutes to total
-		//Display total minutes exercised to the screen
-		//Repeat until the user quits
+		static void Main()
+		{
+			int runningTotal = 0;
+			bool keepGoing = true;
+			
+			while(keepGoing)
+			{
+				//1.
+				Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+				//System.Console.Write() - System = namespace, Console = class, Write = Method
+				//console.write = printing params to the console
+				
+				string entry = Console.ReadLine();
+				if (entry == "quit")
+				{
+					keepGoing = false;
+				}
+				else
+				{
+					//2.
+					int minutes = int.Parse(entry);
+					//extracing the value from entry which is a string and converting it to an integer
+					runningTotal = runningTotal + minutes;
+
+					//3.
+					Console.WriteLine("You've entered " + runningTotal + " minutes.");
+				}
+			}
+			Console.WriteLine("Goodbye");
+		}
 	}
 }
